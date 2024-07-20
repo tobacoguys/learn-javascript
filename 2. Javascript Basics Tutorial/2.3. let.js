7692ec70047e2831b605877baefe3333dda93567
tobacoguys <dungchelsea1723@gmail.com>
2024-07-20 23:51:20 +0700

    let.js

---
+++ 2. Javascript Basics Tutorial/2.3. let.js
@@ -0,0 +1,15 @@
+//The JavaScript let statement is used to declare a variable. With the let statement, we can declare a variable that is block-scoped. This mean a variable declared with let is only accessible within the block of code in which it is defined.
+
+//The let keyword was introduced in the ES6 (2015) version of JavaScript. It is an alternative to the var keyword.
+
+//The main reason behind introducing the let keyword is to improve the scoping behaviors of variables and the safety of the code.
+
+//Variable Declaration with let statement
+//Following is the syntax to declare a variable with let statement −
+
+let var_name = value
+//Let's have a look at some examples for variable declaration with let.
+
+let name = "John";
+let age = 35;
+let x = true;
